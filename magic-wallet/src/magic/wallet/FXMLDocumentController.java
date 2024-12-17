4ecf1b7c2c0b0f8818e783adac5ddde2401ad8a1
The Kaucher <muhammadkaucher018@gmail.com>
2024-12-17 13:24:34 +0600

    ewfwef

--- magic-wallet/src/magic/wallet/FXMLDocumentController.java
+++ magic-wallet/src/magic/wallet/FXMLDocumentController.java
@@ -4,12 +4,20 @@
  */
 package magic.wallet;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -19,8 +27,11 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    
     @FXML
+    private TextField email;
+    @FXML
+    private PasswordField pass;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -30,5 +41,22 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+
+    private void loginbutton(ActionEvent event) throws IOException {
+        
+         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        
+        
+        
+        
+        Scene scene = new Scene(root);
+        Stage stage= new Stage();
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("Profile");
+        
+    }
+
     
 }
